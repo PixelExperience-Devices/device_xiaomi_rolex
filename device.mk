ef4836923794346abd0854f7b11767931ff60bdd
dandyfarid <yuukoito98@gmail.com>
2019-11-25 08:33:42 +0000

    rolex: Remove lineage stuff
    Change-Id: I756be532a7eb4d04700a98d6e71dd7ff97923f76

--- device.mk
+++ device.mk
@@ -19,8 +19,7 @@ DEVICE_PATH := device/xiaomi/rolex
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
